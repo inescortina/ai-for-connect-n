04c52fd15703455af857e273dd02633894d765de
Naoki Ueda <naoki.ueda05@thehutgroup.com>
2023-01-12 13:19:34 +0000

    added minimax and scoreposition of the center column

--- src/main/java/com/thg/accelerator23/connectn/ai/ultimatewinner/UltimateWinner.java
+++ src/main/java/com/thg/accelerator23/connectn/ai/ultimatewinner/UltimateWinner.java
@@ -2,7 +2,10 @@ package com.thg.accelerator23.connectn.ai.ultimatewinner;
 
 import com.thehutgroup.accelerator.connectn.player.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 
 public class UltimateWinner extends Player {
@@ -11,85 +14,80 @@ public class UltimateWinner extends Player {
     super(counter, UltimateWinner.class.getName());
   }
 
-  public MinimaxReturn minimax(Board board, int depth, boolean maximizingPlayer) {
-    BoardAnalyser boardAnalyser = new BoardAnalyser(board.getConfig());
-    GameState gameState = boardAnalyser.calculateGameState(board);
-    if (depth == 0 || gameState.isEnd()) {
-      if (gameState.isEnd()){
-        if (winningMove(board, "X")) {
-          return new MinimaxReturn( -1,1000000000);
-        } else if (winningMove(board, "O")) {
-          return new MinimaxReturn( -1,-1000000000);
-        } else {
-          return new MinimaxReturn(-1, 0);
-        }
-      } else {
-        return new MinimaxReturn(-1, scorePosition());
-      }
+  public int scorePosition (Board board, Counter counter) {
+    counter = this.getCounter();
+    System.out.println(counter);
+    int score = 0;
+    int centerPosition = board.getConfig().getWidth()/2 - 1;
+    System.out.println(centerPosition);
+    ArrayList<Counter> centerArray = new ArrayList<>();
+    for (int n=0; n < board.getConfig().getHeight(); n++) {
+      centerArray.add(board.getCounterAtPosition(new Position(centerPosition, n)));
     }
-    if (maximizingPlayer) {
-      int value = 1000000000;
-      Random rand = new Random();
-      int upperbound = 10;
-      int randomColumn = rand.nextInt(upperbound);
-      int column = randomColumn;
-      for (int i=0; i < board.getConfig().getWidth(); i++) {
-        if (!board.hasCounterAtPosition(new Position(i, board.getConfig().getHeight()-1))){
-          Board boardCopy = new Board(board, )
-          placePiece
-          int newScore = minimax(boardCopy, depth-1, true).getValue();
-          if (newScore > value) {
-            value = newScore;
-            column = i;
-          }
-        };
-      } return new MinimaxReturn(column, value);
-    } else {
-      int value = -1000000000;
-      Random rand = new Random();
-      int upperbound = 10;
-      int randomColumn = rand.nextInt(upperbound);
-      int column = randomColumn;
-      for (int i=0; i < board.getConfig().getWidth(); i++) {
-        if (!board.hasCounterAtPosition(new Position(i, board.getConfig().getHeight()-1))){
-          placePiece
-          int newScore = minimax(boardCopy, depth-1, false).getValue();
-          if (newScore < value) {
-            value = newScore;
-            column = i;
-          }
-        };
-      } return new MinimaxReturn(column, value);
-    }
+    int centerCount = Collections.frequency(centerArray, counter);
+    System.out.println(centerCount);
+    score += centerCount * 3;
+    return score;
 
   }
+
+//  public MinimaxReturn minimax(Board board, int depth, boolean maximizingPlayer) {
+//    BoardAnalyser boardAnalyser = new BoardAnalyser(board.getConfig());
+//    GameState gameState = boardAnalyser.calculateGameState(board);
+//    if (depth == 0 || gameState.isEnd()) {
+//      if (gameState.isEnd()){
+//        if (winningMove(board, "X")) {
+//          return new MinimaxReturn( -1,1000000000);
+//        } else if (winningMove(board, "O")) {
+//          return new MinimaxReturn( -1,-1000000000);
+//        } else {
+//          return new MinimaxReturn(-1, 0);
+//        }
+//      } else {
+//        return new MinimaxReturn(-1, scorePosition());
+//      }
+//    }
+//    if (maximizingPlayer) {
+//      int value = 1000000000;
+//      int column = 0;
+//      for (int i=0; i < board.getConfig().getWidth(); i++) {
+//        if (!board.hasCounterAtPosition(new Position(i, board.getConfig().getHeight()-1))){
+//          Board boardCopy = new Board(board, i, Counter);
+//          int newScore = minimax(boardCopy, depth-1, true).getValue();
+//          if (newScore > value) {
+//            value = newScore;
+//            column = i;
+//          }
+//        };
+//      } return new MinimaxReturn(column, value);
+//    } else {
+//      int value = -1000000000;
+//      int column = 0;
+//      for (int i=0; i < board.getConfig().getWidth(); i++) {
+//        if (!board.hasCounterAtPosition(new Position(i, board.getConfig().getHeight()-1))){
+//          Board boardCopy = new Board(board, i, Counter);
+//          int newScore = minimax(boardCopy, depth-1, false).getValue();
+//          if (newScore < value) {
+//            value = newScore;
+//            column = i;
+//          }
+//        };
+//      } return new MinimaxReturn(column, value);
+//    }
+//
+//  }
 
   @Override
   public int makeMove(Board board) {
     //TODO: some crazy analysis
     //TODO: make sure said analysis uses less than 2G of heap and returns within 10 seconds on whichever machine is running it
-    minimax(board, depth, whichTurn)
-      if depth is 0 or the game is over:
-        return None, score
 
-      if it is your turn
-            for ( i<width){
-              if(isvalid){
-                1. make a copy
-                2. place a counter
-                3. call minimax() as opponent side
-                4. if the score is value
-              }
-            }
-
-
+    int score = scorePosition(board, Counter.O);
+    System.out.println(score);
 
     Random rand = new Random();
-
-
     int upperbound = 10;
     int randomColumn = rand.nextInt(upperbound);
-    System.out.println();
     if (!board.hasCounterAtPosition(new Position(4, board.getConfig().getHeight()-1))){
       return 4;
     } else {
---
+++ src/test/java/com/thg/accelerator21/connectn/ai/name/UltimateWinnerTest.java
@@ -0,0 +1,25 @@
+package com.thg.accelerator21.connectn.ai.name;
+
+import com.thehutgroup.accelerator.connectn.player.Board;
+import com.thehutgroup.accelerator.connectn.player.Counter;
+import com.thehutgroup.accelerator.connectn.player.GameConfig;
+import com.thehutgroup.accelerator.connectn.player.InvalidMoveException;
+import com.thg.accelerator23.connectn.ai.ultimatewinner.UltimateWinner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Assertions;
+
+
+//class UltimateWinnerTest {
+//    @Test
+//    @DisplayName("Return score")
+//    public void scorePositionReturnInt(){
+//        Board board = new Board(new GameConfig(10, 8, 4));
+//        try {board = new Board(board, 4, Counter.X);}
+//        catch (InvalidMoveException e){
+//
+//        }
+//        Assertions.assertEquals(new UltimateWinner(Counter.O).scorePosition(board, Counter.O), 3);
+//    }
+//
+//}
